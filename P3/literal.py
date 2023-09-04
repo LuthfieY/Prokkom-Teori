950554716f8761434227451f1e3b629bb3a0a903
Luthfie Y <luthfieyannuardy@mail.ugm.ac.id>
2023-09-04 08:51:25 +0700

    upload p3

---
+++ P3/literal.py
@@ -0,0 +1,37 @@
+a = 11,11
+b = 11_111_111
+c = 11.111
+d = 11,111,111
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+# print(11.111.111) Error
+
+print()
+print("Octal 0o123: ", 0o123)
+print("Hexa 0x123: ", 0x123)
+print(type(0o123))
+print(type(0x123))
+#Fungsi print() langsung mengkonversikannya ke bentuk decimal
+print()
+a = 3e8 #e = eksponen 3 x 10^8
+b = 6.62607e-34
+#Mengikuti kaidah signifikan x basis^eksponen /notasi saintifik
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+print(0.000000000000000000001)
+
+print('\nSaya pengen "tidur"\n')
+
+print(1 > 0)
+print(1 < 0)
+print(True > False)
+print(True < False)
