b4c343a05a058b31a35f4df57fc1836eeac58498
Luthfie Y <luthfieyannuardy@mail.ugm.ac.id>
2023-09-25 08:40:38 +0700

    ADD: Latihan P6

---
+++ P6/lat1.py
@@ -0,0 +1,3 @@
+x = float(input("Masukan nilai x: "))
+y = 1/(x+1/(x+1/(x+1/x)))
+print("y =", y)
---
+++ P6/lat2.py
@@ -0,0 +1,3 @@
+rad = float(input("Masukan nilai dalam rad/s: "))
+hz = rad * 0.15922
+print(rad, "rad/s =", hz, "Hz")
